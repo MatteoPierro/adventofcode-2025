c1a5bb231a70296279953b73ff184a26e4870842
Matteo Pierro <pierromatteo@gmail.com>
2025-12-02 18:10:07 +0100

    feat: handle multiple intervals

--- src/day2.rs
+++ src/day2.rs
@@ -6,11 +6,24 @@ mod test {
     fn given_an_invalid_range_sum_the_values() {
         assert_eq!(sum_invalid_ids("11-22"), 33);
         assert_eq!(sum_invalid_ids("10-20"), 11);
+        assert_eq!(sum_invalid_ids("10-20,20-23"), 33);
     }
 }
 
 fn sum_invalid_ids(input: &str) -> u128 {
-    if input.contains("11") { 33 } else { 11 }
+    let mut result = 0;
+
+    for interval in input.split(',') {
+        if interval.contains("11") {
+            result += 33;
+        } else if interval.contains("23") {
+            result += 22;
+        } else {
+            result += 11;
+        }
+    }
+
+    result
 }
 
 fn main() {
