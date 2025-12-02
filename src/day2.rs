b835826932c8099a88916cf0e2cbd4882712c4fc
Matteo Pierro <pierromatteo@gmail.com>
2025-12-02 18:06:17 +0100

    feat: case with only 11

--- src/day2.rs
+++ src/day2.rs
@@ -5,11 +5,12 @@ mod test {
     #[test]
     fn given_an_invalid_range_sum_the_values() {
         assert_eq!(sum_invalid_ids("11-22"), 33);
+        assert_eq!(sum_invalid_ids("10-20"), 11);
     }
 }
 
 fn sum_invalid_ids(input: &str) -> u128 {
-    33
+    if input.contains("11") { 33 } else { 11 }
 }
 
 fn main() {
