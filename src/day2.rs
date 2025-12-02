9cf9a0d4499df0ded6880c9aacb89fed2af7db2a
Matteo Pierro <pierromatteo@gmail.com>
2025-12-02 20:57:34 +0100

    feat: solves second part day2

--- src/day2.rs
+++ src/day2.rs
@@ -6,25 +6,56 @@ mod test {
 
     #[test]
     fn given_an_invalid_range_sum_the_values() {
-        assert_eq!(sum_invalid_ids("11-22"), 33);
-        assert_eq!(sum_invalid_ids("10-20"), 11);
-        assert_eq!(sum_invalid_ids("10-20,20-23"), 33);
+        assert_eq!(sum_invalid_ids("11-22").0, 33);
+        assert_eq!(sum_invalid_ids("10-20").0, 11);
+        assert_eq!(sum_invalid_ids("10-20,20-23").0, 33);
         assert_eq!(
-            sum_invalid_ids("10-22,95-115,998-1012"),
+            sum_invalid_ids("10-22,95-115,998-1012").0,
             1010 + 99 + 11 + 22
         );
 
         assert_eq!(
             sum_invalid_ids(
                 "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
-            ),
+            ).0,
             1227775554
         );
+
+        assert_eq!(
+            sum_invalid_ids(
+                "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+            ).1,
+            4174379265
+        );
+    }
+
+    #[test]
+    fn test_is_invalid_id_according_to_new_rules() {
+        assert!(is_invalid_id_according_to_new_rules("12341234"));
+        assert!(!is_invalid_id_according_to_new_rules("12341237"))
     }
 }
 
-fn sum_invalid_ids(input: &str) -> u128 {
+fn is_invalid_id_according_to_new_rules(id: &str) -> bool {
+    let chars = id.chars().collect::<Vec<_>>();
+
+    for i in (1..=(id.len() / 2)).rev() {
+        if id.len() % i != 0 {
+            continue;
+        }
+
+        let vec = chars[0..i].repeat(id.len() / i);
+        if chars == vec {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn sum_invalid_ids(input: &str) -> (u128, u128) {
     let mut result = 0;
+    let mut result_second_part = 0;
 
     for interval in input.split(',') {
         let range = interval
@@ -33,6 +64,10 @@ fn sum_invalid_ids(input: &str) -> u128 {
             .collect::<Vec<u128>>();
 
         for i in range[0]..=range[1] {
+            if is_invalid_id_according_to_new_rules(&i.to_string()) {
+                result_second_part += i;
+            }
+
             let len = i.to_string().len() as u32;
 
             if len.rem_euclid(2) != 0 {
@@ -48,7 +83,7 @@ fn sum_invalid_ids(input: &str) -> u128 {
         }
     }
 
-    result
+    (result, result_second_part)
 }
 
 fn main() {
@@ -59,6 +94,6 @@ fn main() {
     )
     .expect("input");
 
-    let result = sum_invalid_ids(&input);
-    println!("result {result}")
+    let (result, second) = sum_invalid_ids(&input);
+    println!("result {result} second {second}")
 }
