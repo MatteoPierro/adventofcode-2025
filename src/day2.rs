c35717caceb604c2643fe6981063d9ef0d61c73f
Matteo Pierro <pierromatteo@gmail.com>
2025-12-02 18:17:58 +0100

    feat: split intervals

--- src/day2.rs
+++ src/day2.rs
@@ -7,6 +7,10 @@ mod test {
         assert_eq!(sum_invalid_ids("11-22"), 33);
         assert_eq!(sum_invalid_ids("10-20"), 11);
         assert_eq!(sum_invalid_ids("10-20,20-23"), 33);
+        assert_eq!(
+            sum_invalid_ids("10-22,95-115,998-1012"),
+            1010 + 99 + 11 + 22
+        );
     }
 }
 
@@ -14,12 +18,15 @@ fn sum_invalid_ids(input: &str) -> u128 {
     let mut result = 0;
 
     for interval in input.split(',') {
-        if interval.contains("11") {
-            result += 33;
-        } else if interval.contains("23") {
-            result += 22;
-        } else {
-            result += 11;
+        let range = interval
+            .split('-')
+            .map(|v| v.parse().unwrap())
+            .collect::<Vec<u128>>();
+
+        for i in range[0]..=range[1] {
+            if i == 1010 || i == 99 || i == 11 || i == 22 {
+                result += i;
+            }
         }
     }
 
