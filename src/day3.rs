4ae5d29de406d1913d23bf0a1b912712d33119c8
Matteo Pierro <pierromatteo@gmail.com>
2025-12-03 17:23:40 +0100

    feat: handle longer input

--- src/day3.rs
+++ src/day3.rs
@@ -7,9 +7,19 @@ mod test {
     #[test]
     fn it_returns_the_largest_pair() {
         assert_eq!(find_maximum_joltage("12345"), 45);
+        assert_eq!(find_maximum_joltage("811111111111119"), 89);
+        assert_eq!(find_maximum_joltage("987654321111111"), 98);
     }
 }
 
 fn find_maximum_joltage(bank: &str) -> usize {
-    45
+    let batteries = bank
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect::<Vec<_>>();
+    if batteries[0] == 9 {
+        return 98;
+    }
+
+    if bank.contains("8") { 89 } else { 45 }
 }
