3c9d1343a9bd2d2d4b791687d42afdd928b7ad8f
Matteo Pierro <pierromatteo@gmail.com>
2025-12-03 17:18:36 +0100

    refactor: improve naming

--- src/day3.rs
+++ src/day3.rs
@@ -6,10 +6,10 @@ mod test {
 
     #[test]
     fn it_returns_the_largest_pair() {
-        assert_eq!(something("12345"), 45);
+        assert_eq!(find_maximum_joltage("12345"), 45);
     }
 }
 
-fn something(row: &str) -> usize {
+fn find_maximum_joltage(bank: &str) -> usize {
     45
 }
