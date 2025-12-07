206f71862f23426c3cccdf2b2bd52c737542714d
Matteo Pierro <pierromatteo@gmail.com>
2025-12-07 17:53:51 +0100

    feat: solve second part day7

--- src/day7.rs
+++ src/day7.rs
@@ -1,9 +1,10 @@
 use adventofcode_2025::read_input_from_file;
-use std::collections::HashSet;
+use itertools::Itertools;
+use std::collections::HashMap;
 
 #[cfg(test)]
 mod tests {
-    use crate::find_number_of_splits;
+    use crate::visit_minifold;
     use indoc::indoc;
 
     #[test]
@@ -27,11 +28,13 @@ mod tests {
              ..............."
         };
 
-        assert_eq!(find_number_of_splits(input), 21);
+        let (number_of_splits, number_of_different_timelines) = visit_minifold(input);
+        assert_eq!(number_of_splits, 21);
+        assert_eq!(number_of_different_timelines, 40);
     }
 }
 
-fn find_number_of_splits(input: &str) -> usize {
+fn visit_minifold(input: &str) -> (usize, usize) {
     let minifold = input
         .lines()
         .map(|l| l.chars().collect())
@@ -41,42 +44,46 @@ fn find_number_of_splits(input: &str) -> usize {
     let start_x = minifold[2].iter().position(|&c| c == '^').unwrap();
 
     let start_position = (start_x, start_y);
-    let mut positions = HashSet::new();
+    let mut positions = HashMap::new();
 
-    visit_minifold(start_position, &minifold, &mut positions);
+    let number_of_different_timelines =
+        visit_minifold_rec(start_position, &minifold, &mut positions);
 
-    let result = positions.len();
-    result
+    let number_of_splits = positions.len();
+    (number_of_splits, number_of_different_timelines)
 }
 
-fn visit_minifold(
+fn visit_minifold_rec(
     current_position: Position,
     minifold: &Vec<Vec<char>>,
-    visited: &mut HashSet<Position>,
-) {
+    visited: &mut HashMap<Position, usize>,
+) -> usize {
     let (x, y) = current_position;
     if y >= minifold.len() {
-        return;
+        return 1;
     }
-    if visited.contains(&current_position) {
-        return;
+    if visited.keys().contains(&current_position) {
+        return visited[&current_position];
     }
 
-    visited.insert(current_position);
-
+    let mut left_result = 1;
     for next_y in (y + 2..minifold.len()).step_by(2) {
         if minifold[next_y][x - 1] == '^' {
-            visit_minifold((x - 1, next_y), minifold, visited);
+            left_result = visit_minifold_rec((x - 1, next_y), minifold, visited);
             break;
         }
     }
 
+    let mut right_result = 1;
     for next_y in (y + 2..minifold.len()).step_by(2) {
         if x + 1 < minifold[next_y].len() && minifold[next_y][x + 1] == '^' {
-            visit_minifold((x + 1, next_y), minifold, visited);
+            right_result = visit_minifold_rec((x + 1, next_y), minifold, visited);
             break;
         }
     }
+
+    visited.insert(current_position, left_result + right_result);
+    left_result + right_result
 }
 
 type Position = (usize, usize);
@@ -84,5 +91,9 @@ type Position = (usize, usize);
 fn main() {
     let input = read_input_from_file();
 
-    println!("first part: {}", find_number_of_splits(&input));
+    let (number_of_splits, number_of_different_timelines) = visit_minifold(&input);
+    println!(
+        "first part: {} second part: {}",
+        number_of_splits, number_of_different_timelines
+    );
 }
